f5949b0de1bf973f4b05f544cc697bd2426df0d1
Michele Bertoli <michele@berto.li>
2017-04-16 22:34:24 +0100

    0.8.0

--- dist/react-count-to.js
+++ dist/react-count-to.js
@@ -4,121 +4,165 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var CountTo = _react2.default.createClass({
-  displayName: 'CountTo',
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var propTypes = {
+  from: _propTypes2.default.number,
+  to: _propTypes2.default.number.isRequired,
+  speed: _propTypes2.default.number.isRequired,
+  delay: _propTypes2.default.number,
+  onComplete: _propTypes2.default.func,
+  digits: _propTypes2.default.number,
+  className: _propTypes2.default.string,
+  tagName: _propTypes2.default.string,
+  children: _propTypes2.default.func
+};
+
+var defaultProps = {
+  from: 0,
+  delay: 100,
+  digits: 0,
+  tagName: 'span'
+};
+
+var CountTo = function (_Component) {
+  _inherits(CountTo, _Component);
+
+  function CountTo(props) {
+    _classCallCheck(this, CountTo);
+
+    var _this = _possibleConstructorReturn(this, (CountTo.__proto__ || Object.getPrototypeOf(CountTo)).call(this));
+
+    var from = props.from;
 
 
-  propTypes: {
-    from: _react2.default.PropTypes.number,
-    to: _react2.default.PropTypes.number.isRequired,
-    speed: _react2.default.PropTypes.number.isRequired,
-    delay: _react2.default.PropTypes.number,
-    onComplete: _react2.default.PropTypes.func,
-    digits: _react2.default.PropTypes.number,
-    className: _react2.default.PropTypes.string,
-    tagName: _react2.default.PropTypes.string,
-    children: _react2.default.PropTypes.func
-  },
-
-  getDefaultProps: function getDefaultProps() {
-    return {
-      from: 0,
-      delay: 100,
-      digits: 0,
-      tagName: 'span'
-    };
-  },
-  getInitialState: function getInitialState() {
-    var from = this.props.from;
-
-
-    return {
+    _this.state = {
       counter: from
     };
-  },
-  componentDidMount: function componentDidMount() {
-    this.start();
-  },
-  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-    var _props = this.props;
-    var from = _props.from;
-    var to = _props.to;
 
+    _this.start = _this.start.bind(_this);
+    _this.clear = _this.clear.bind(_this);
+    _this.next = _this.next.bind(_this);
+    return _this;
+  }
 
-    if (nextProps.to !== to || nextProps.from !== from) {
+  _createClass(CountTo, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
       this.start();
     }
-  },
-  componentWillUnmount: function componentWillUnmount() {
-    this.clear();
-  },
-  start: function start() {
-    var _this = this;
+  }, {
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(nextProps) {
+      var _props = this.props,
+          from = _props.from,
+          to = _props.to;
 
-    this.clear();
-    this.setState(this.getInitialState(), function () {
-      var _props2 = _this.props;
-      var delay = _props2.delay;
-      var speed = _props2.speed;
-      var to = _props2.to;
-      var counter = _this.state.counter;
 
-      _this.loopsCounter = 0;
-      _this.loops = Math.ceil(speed / delay);
-      _this.increment = (to - counter) / _this.loops;
-      _this.interval = setInterval(_this.next, delay);
-    });
-  },
-  next: function next() {
-    var _this2 = this;
-
-    if (this.loopsCounter < this.loops) {
-      this.loopsCounter++;
-      this.setState(function (_ref) {
-        var counter = _ref.counter;
-        return {
-          counter: counter + _this2.increment
-        };
-      });
-    } else {
-      var onComplete = this.props.onComplete;
-
-      this.clear();
-
-      if (onComplete) {
-        onComplete();
+      if (nextProps.to !== to || nextProps.from !== from) {
+        this.start();
       }
     }
-  },
-  clear: function clear() {
-    clearInterval(this.interval);
-  },
-  render: function render() {
-    var _props3 = this.props;
-    var className = _props3.className;
-    var digits = _props3.digits;
-    var Tag = _props3.tagName;
-    var fn = _props3.children;
-    var counter = this.state.counter;
-
-    var value = counter.toFixed(digits);
-
-    if (fn) {
-      return fn(value);
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      this.clear();
     }
+  }, {
+    key: 'start',
+    value: function start() {
+      var _this2 = this;
 
-    return _react2.default.createElement(
-      Tag,
-      { className: className },
-      value
-    );
-  }
-});
+      this.clear();
+      var from = this.props.from;
+
+      this.setState({
+        counter: from
+      }, function () {
+        var _props2 = _this2.props,
+            delay = _props2.delay,
+            speed = _props2.speed,
+            to = _props2.to;
+        var counter = _this2.state.counter;
+
+        _this2.loopsCounter = 0;
+        _this2.loops = Math.ceil(speed / delay);
+        _this2.increment = (to - counter) / _this2.loops;
+        _this2.interval = setInterval(_this2.next, delay);
+      });
+    }
+  }, {
+    key: 'next',
+    value: function next() {
+      var _this3 = this;
+
+      if (this.loopsCounter < this.loops) {
+        this.loopsCounter++;
+        this.setState(function (_ref) {
+          var counter = _ref.counter;
+          return {
+            counter: counter + _this3.increment
+          };
+        });
+      } else {
+        var onComplete = this.props.onComplete;
+
+        this.clear();
+
+        if (onComplete) {
+          onComplete();
+        }
+      }
+    }
+  }, {
+    key: 'clear',
+    value: function clear() {
+      clearInterval(this.interval);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _props3 = this.props,
+          className = _props3.className,
+          digits = _props3.digits,
+          Tag = _props3.tagName,
+          fn = _props3.children;
+      var counter = this.state.counter;
+
+      var value = counter.toFixed(digits);
+
+      if (fn) {
+        return fn(value);
+      }
+
+      return _react2.default.createElement(
+        Tag,
+        { className: className },
+        value
+      );
+    }
+  }]);
+
+  return CountTo;
+}(_react.Component);
+
+CountTo.propTypes = propTypes;
+CountTo.defaultProps = defaultProps;
 
 exports.default = CountTo;
